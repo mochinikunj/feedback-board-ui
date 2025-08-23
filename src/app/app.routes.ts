b4d6eefd34bbb5878d43953875507466d05ea70f
Nikunj Mochi <mochinikunj2@gmail.com>
2025-08-23 11:43:32 +0530

    Initial setup of components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { FeedbackComponent } from './feedback/feedback.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: FeedbackComponent,
+  },
+];
