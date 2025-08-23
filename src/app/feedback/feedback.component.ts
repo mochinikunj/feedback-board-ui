0fbe72303de332843fcc7538d83fad8c6c1a3cfa
Nikunj Mochi <mochinikunj2@gmail.com>
2025-08-23 11:45:04 +0530

    Applying prettier

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -5,8 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './feedback.component.html',
-  styleUrl: './feedback.component.css'
+  styleUrl: './feedback.component.css',
 })
-export class FeedbackComponent {
-
-}
+export class FeedbackComponent {}
